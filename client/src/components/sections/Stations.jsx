83852b9e073b55387b006f0f6b045062eff1a533
Mahek Jain <mahekmj123@gmail.com>
2024-02-24 22:48:04 +0530

    map ui

--- client/src/components/sections/Stations.jsx
+++ client/src/components/sections/Stations.jsx
@@ -4,6 +4,9 @@ import { MdOutlineElectricBolt, MdCancelPresentation } from "react-icons/md";
 import Car from "../../assets/Car.png";
 import EV from "../../assets/evicon.png";
 import Line from "../../charts/Line";
+import { BsLightningChargeFill } from "react-icons/bs";
+import { RiGasStationFill } from "react-icons/ri";
+import { FaPhoneAlt } from "react-icons/fa";
 
 const Stations = () => {
   const [evcar, setEvcar] = useState({
@@ -19,7 +22,7 @@ const Stations = () => {
     status: "Good",
   });
   let map;
-const [stations, setStations] = useState([]);
+  const [stations, setStations] = useState([]);
   useEffect(() => {
     // Load the Google Maps script
     const script = document.createElement("script");
@@ -48,49 +51,48 @@ const [stations, setStations] = useState([]);
     new google.maps.Marker({
       position: { lat: 37.7937, lng: -122.3965 },
       map: map,
-       icon: {
-         url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
-       },
+      icon: {
+        url: "http://maps.google.com/mapfiles/ms/icons/red-dot.png",
+      },
     });
   }
 
- function addMarker(station) {
-   console.log("Adding marker for station:", station);
-   var marker = new google.maps.Marker({
-     position: {
-       lat: station.location.latitude,
-       lng: station.location.longitude,
-     },
-     map: map,
-     icon: {
-       url: EV,
-       scaledSize: new google.maps.Size(50, 70),
-     },
-   });
-console.log(station.formattedAddress)
-var infowindow = new google.maps.InfoWindow({
-  content: `<div style='height:100px; width:200px; color:black'>
+  function addMarker(station) {
+    console.log("Adding marker for station:", station);
+    var marker = new google.maps.Marker({
+      position: {
+        lat: station.location.latitude,
+        lng: station.location.longitude,
+      },
+      map: map,
+      icon: {
+        url: EV,
+        scaledSize: new google.maps.Size(50, 70),
+      },
+    });
+    console.log(station.formattedAddress);
+    var infowindow = new google.maps.InfoWindow({
+      content: `<div style='height:100px; width:200px; color:black'>
       ${station.formattedAddress}<br>
       Open Now: ${station?.currentOpeningHours?.openNow}<br>
       Open: Day ${station?.currentOpeningHours?.periods[0]?.open?.day}, Hour ${
-    station?.currentOpeningHours?.periods[0]?.open?.hour
-  }, Minute ${station?.currentOpeningHours?.periods[0]?.open?.minute}<br>
+        station?.currentOpeningHours?.periods[0]?.open?.hour
+      }, Minute ${station?.currentOpeningHours?.periods[0]?.open?.minute}<br>
       Close: Day ${
         station?.currentOpeningHours?.periods[0]?.close?.day
       }, Hour ${
-    station?.currentOpeningHours?.periods[0]?.close?.hour
-  }, Minute ${station?.currentOpeningHours?.periods[0]?.close?.minute}
+        station?.currentOpeningHours?.periods[0]?.close?.hour
+      }, Minute ${station?.currentOpeningHours?.periods[0]?.close?.minute}
        Weekday Descriptions:<br>
       ${station?.currentOpeningHours?.weekdayDescriptions?.join("<br>")}
     </div>`,
-});
+    });
 
-
- marker.addListener("click", function () {
-   infowindow.open(map, marker);
-   console.log(station.formattedAddress);
- });
- }
+    marker.addListener("click", function () {
+      infowindow.open(map, marker);
+      console.log(station.formattedAddress);
+    });
+  }
   const getNearbyStations = (userLocation, map) => {
     const url = "https://places.googleapis.com/v1/places:searchNearby";
     const apiKey = "AIzaSyAGHFR3hfwbf_yGyfkPFZ7aSfj7Jr7RDfg"; // replace with your API key
@@ -130,52 +132,102 @@ var infowindow = new google.maps.InfoWindow({
         console.error("Error:", error);
       });
   };
-return (
-  <div style={{ position: "relative" }}>
-    <div id="map" style={{ height: "900px", width: "100%" }}></div>
-    <div
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width: "30%",
-        height: "900px",
-        overflow: "auto",
-        color: "black",
-        backgroundColor: "rgba(255, 255, 255, 0.8)", // Semi-transparent background
-      }}
-    >
-      {stations?.map((station, index) => (
-        <div
-          className="text-white flex flex-col gap-4 bg-black border-2 border-white"
-          key={index}
-        >
-          <h2>{station?.displayName?.text}</h2>
-          <p>{station?.primaryType}</p>
-          <p>{station?.formattedAddress}</p>
-          <p>{station?.currentOpeningHours?.openNow}</p>
-          <li>{station?.currentOpeningHours?.weekdayDescriptions}</li>
-          <p>{station?.internationalPhoneNumber}</p>
-          <p>{station?.websiteUri}</p>
-          <p>Connector Count: {station?.evChargeOptions?.connectorCount}</p>
-          {station?.evChargeOptions?.connectorAggregation?.map(
-            (connector, i) => (
-              <div key={i}>
-                <p>Type: {connector.type}</p>
-                <p>Max Charge Rate: {connector.maxChargeRateKw} kW</p>
-                <p>Count: {connector.count}</p>
-                <p>Available Count: {connector.availableCount}</p>
-                <p>Out of Service Count: {connector.outOfServiceCount}</p>
-                <p>Last Update Time: {connector.availabilityLastUpdateTime}</p>
+  const formatPrimaryType = (primaryType) => {
+    if (!primaryType) return "";
+    return primaryType
+      .split("_")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(" ");
+  };
+
+  return (
+    <div style={{ position: "relative" }}>
+      <div id="map" style={{ height: "900px", width: "100%" }}></div>
+      <div
+        style={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "33.33%",
+          height: "900px",
+          overflow: "auto",
+          color: "black",
+          backgroundColor: "rgba(255, 255, 255, 0.8)", // Semi-transparent background
+        }}
+      >
+        {stations?.map((station, index) => (
+          <div
+            className="text-white p-5 rounded-md overflow-x-hidden flex flex-col  bg-black border-2 border-white"
+            key={index}
+          >
+            <div className="flex justify-between items-center">
+              <a
+                className="mt-2 font-medium text-md"
+                href={station?.websiteUri}
+              >
+                {station?.displayName?.text}
+              </a>
+              <p className="text-[#44dba0] flex">
+                <RiGasStationFill className="mr-2" size={24} />
+                {station?.evChargeOptions?.connectorCount}
+              </p>
+            </div>
+            <p className="my-2 text-[#575757] font-medium text-sm">
+              {formatPrimaryType(station?.primaryType)}
+            </p>{" "}
+            <div className="flex items-center">
+              <BsLightningChargeFill
+                className="mr-2 text-[#44dba0]"
+                size={20}
+              />
+              <p className="">{station?.formattedAddress}</p>
+            </div>
+            <p>{station?.currentOpeningHours?.openNow}</p>
+            {/* <p className="py-4">
+              {Object.entries(
+                station?.currentOpeningHours?.weekdayDescriptions || {}
+              ).map(([day, hours]) => (
+                <span key={day} className="text-sm py-2 text-[#8d8787]">
+                  {hours}
+                  <br />
+                </span>
+              ))}
+            </p> */}
+            <p className="my- text-[#44dba0]">Open 24 Hours</p>
+            <div className="flex items-center">
+            <FaPhoneAlt size={16} />
+            <p className="my-2 ml-2">{station?.internationalPhoneNumber}</p>
               </div>
-            )
-          )}
-          <button>Get Directions</button>
-        </div>
-      ))}
+            
+            {/* <p>{station?.websiteUri}</p> */}
+            {station?.evChargeOptions?.connectorAggregation?.map(
+              (connector, i) => (
+                <div key={i} className="bg-gray-800 p-4 rounded-md mb-4">
+                  <p className="text-[#44dba0]">
+                    Type: {formatPrimaryType(connector.type)}
+                  </p>
+                  <p className="text-[#44dba0]">
+                    Max Charge Rate: {connector.maxChargeRateKw} kW
+                  </p>
+                  {connector.availabilityLastUpdateTime && (
+                    <p className="text-white text-sm">
+                      Last Update Time: {connector.availabilityLastUpdateTime}
+                    </p>
+                  )}
+                </div>
+              )
+            )}
+            <button
+              type="submit"
+              className="bg-[#44dba0] text-white rounded-md px-4 py-2 mr-2"
+            >
+              Get Directions
+            </button>
+          </div>
+        ))}
+      </div>
     </div>
-  </div>
-);
-        }
+  );
+};
 
 export default Stations;
